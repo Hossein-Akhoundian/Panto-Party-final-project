1c250b429000e6b3c253ae876206c6d25bd9df28
m.akhoundian <m.akhoundian@apk-group.net>
2026-08-30 13:44:10 +0330

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Panto-Party-final-project/",
   plugins: [react()],
   resolve: {
     alias: {
